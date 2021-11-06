805d90a887c096f5e354a832f9a1eadc244517f2
akidv <93820991+akidv@users.noreply.github.com>
2021-11-06 11:29:14 +0100

    Add files via upload

---
+++ webCV/main.js
@@ -0,0 +1,17 @@
+
+function changeTheme() {
+    if (document.body.style.backgroundColor === 'white') {
+        document.body.style.backgroundColor = '#cdcdcd';
+    } else {
+        document.body.style.backgroundColor = 'white'; 
+    }
+}
+
+function changeFont() {
+    if (document.body.style.fontFamily === 'Times') {
+        document.body.style.fontFamily = 'Arial';
+    } else {
+        document.body.style.fontFamily = 'Times';
+    }
+    
+}
